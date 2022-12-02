e28f11596d09bc6a582e46525fb29e5b717f2e41
Shakil53 <shakilahmed.dcc@gmail.com>
2022-12-03 01:04:48 +0600

    done album

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PhotoAlbum></PhotoAlbum>
     </div>
   );
 }
+
+function PhotoAlbum() {
+
+  const [photos, setPhotos] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(data => setPhotos(data))
+  },[])
+
+  return (
+    <div>
+      <h1>Total Photos:-{photos.length}</h1>
+      <div className='photo-container'>
+      {
+        photos.map(photo => <Photo title={photo.title} id={photo.id} url={photo.url}></Photo>)
+      }
+      </div>
+      
+    </div>
+  )
+}
+
+function Photo(props) {
+  return (
+    <div className='photo'>
+      <h4>Photo Id:{props.id}</h4>
+      <p>Title:{props.title}</p>
+      <img src={props.url}></img>
+    </div>
+  )
+}
+
 
 export default App;
